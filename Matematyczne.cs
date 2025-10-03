e8fd0384b151c6ff2e924f2b3be6d042e4fe467b
yedips <szymonwaskiewicz200@gmail.com>
2025-10-03 16:36:41 +0200

    Dodanie BubbleSort

--- Matematyczne.cs
+++ Matematyczne.cs
@@ -70,6 +70,24 @@ namespace mATEMATYCZNE
             return n * Silnia(n - 1);
         }
 
+        public int BubbleSort(int[] tablica)
+        {
+            int n = tablica.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (tablica[j] > tablica[j + 1])
+                    {
+                        int temp = tablica[j];
+                        tablica[j] = tablica[j + 1];
+                        tablica[j + 1] = temp;
+                    }
+                }
+            }
+            return 0;
+        }
+
 
     }
 }
